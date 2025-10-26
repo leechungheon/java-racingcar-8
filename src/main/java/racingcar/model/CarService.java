b2b04ed168efc5f46a5206aeffed2a1bb6112b28
leechungheon <leechung6311@gmail.com>
2025-10-26 21:21:45 +0900

    refactor(CarService): 매직 넘버 제거 및 스트림 적용
    CarService 내 검증 메서드들의 일관성을 확보하고 매직 넘버를 상수로
    대체했습니다. 또한, for 루프를 스트림으로 대체하여 코드 간결성을
    높였습니다.
    
    변경 내용:
    - validateDelimiterPosition() 메서드 내부의 for 루프를
      Stream.anyMatch()로 리팩토링하여 들여쓰기 depth를 줄이고 코드 간결성을
      확보했습니다.
    - checkAtLeastTwoCars() 메서드명을 validateMinimumCarCount()로
      변경하여 모든 검증 메서드의 접두사를 'validate'로 통일했습니다.
    - 최소 자동차 개수 '2'를 private static final 상수
      MINIMUM_CAR_COUNT로 추출하여 매직 넘버를 제거하고 유지보수성을
      높였습니다.

--- src/main/java/racingcar/model/CarService.java
+++ src/main/java/racingcar/model/CarService.java
@@ -1,38 +1,41 @@
 package racingcar.model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarService {
-    // 스트림 API로 추후 리팩터링 고려하기
+    private static final int MINIMUM_CAR_COUNT = 2;
     public Cars createCar(String inputCarNames){
         validateNoBlankInCarNames(inputCarNames);
         String[] carArray = inputCarNames.split(",",-1);
         validateDelimiterPosition(carArray);
-        checkAtLeastTwoCars(carArray);
-        List<Car> carList = new ArrayList<>();
-        for (String car : carArray) {
-            carList.add(new Car(car));
-        }
+        validateMinimumCarCount(carArray);
+
+        List<Car> carList = Arrays.stream(carArray)
+                .map(Car::new)
+                .collect(Collectors.toList());
         return new Cars(carList);
     }
 
-    public void validateNoBlankInCarNames(String inputCarNames){
+    private void validateNoBlankInCarNames(String inputCarNames){
         if(inputCarNames.contains(" ")){
             throw new IllegalArgumentException("이름에 공백은 포함할 수 없습니다.");
         }
     }
 
-    public void validateDelimiterPosition(String[] carArray){
-        for(int i=0; i<carArray.length; i++){
-            if(carArray[i].isBlank()){
-                throw new IllegalArgumentException("잘못된 구분자(,)위치는 허용되지 않습니다.");
-            }
+    private void validateDelimiterPosition(String[] carArray){
+        boolean hasBlankEntry = Arrays.stream(carArray)
+                .anyMatch(String::isBlank);
+
+        if(hasBlankEntry){
+            throw new IllegalArgumentException("잘못된 구분자(,)위치는 허용되지 않습니다.");
         }
+
     }
 
-    public void checkAtLeastTwoCars(String[] carArray){
-        if(carArray.length<2){
+    private void validateMinimumCarCount(String[] carArray){
+        if(carArray.length < MINIMUM_CAR_COUNT){
             throw new IllegalArgumentException("자동차는 2대 이상 입력해야 합니다.");
         }
     }
