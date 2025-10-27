268a9a8fd2ab1b5e8505e22dd965a3fe95c3fd7c
leechungheon <leechung6311@gmail.com>
2025-10-27 19:54:57 +0900

    fix(Output): requestCarNames 메서드 요구사항 오타 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,7 +8,7 @@ import racingcar.model.Cars;
 public class OutputView {
     private final String SCORE_VISUALIZATION_CHAR = "-";
     public void requestCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분).");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void requestTryNumber() {
